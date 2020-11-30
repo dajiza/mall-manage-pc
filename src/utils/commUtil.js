780dca0366a96e094bc5a5f1b35321845d4bc8aa
feng <18300671093@163.com>
2020-11-30 18:17:58 +0800

    修改 -- 待付款订单修改价格、运费调整

--- src/utils/commUtil.js
+++ src/utils/commUtil.js
@@ -14,5 +14,47 @@ export default {
             }
         })
         return dealOptions
+    },
+    // 浮点数 + - x
+    numberAdd(a, b) {
+        let c, d, e;
+        try {
+            c = a.toString().split(".")[1].length;
+        } catch (f) {
+            c = 0;
+        }
+        try {
+            d = b.toString().split(".")[1].length;
+        } catch (f) {
+            d = 0;
+        }
+        return e = Math.pow(10, Math.max(c, d)), (this.numberMul(a, e) + this.numberMul(b, e)) / e;
+    },
+
+    numberSub(a, b) {
+        let c, d, e;
+        try {
+            c = a.toString().split(".")[1].length;
+        } catch (f) {
+            c = 0;
+        }
+        try {
+            d = b.toString().split(".")[1].length;
+        } catch (f) {
+            d = 0;
+        }
+        return e = Math.pow(10, Math.max(c, d)), (this.numberMul(a, e) - this.numberMul(b, e)) / e;
+    },
+    numberMul(a, b) {
+        let c = 0,
+            d = a.toString(),
+            e = b.toString();
+        try {
+            c += d.split(".")[1].length;
+        } catch (f) {}
+        try {
+            c += e.split(".")[1].length;
+        } catch (f) {}
+        return Number(d.replace(".", "")) * Number(e.replace(".", "")) / Math.pow(10, c);
     }
 };
